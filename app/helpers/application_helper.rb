83a7fcdbd6eff12f084460374f31879338ff1d6f
Rocco Nicholls <rocco11nicholls@gmail.com>
2021-02-06 16:47:21 -0700

    Formatted time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def timeago(time, options={})
+    return if time.blank?
+
+    date = time.strftime("%b %-d '%y")
+    time = time.strftime("%-l:%M %p")
+    simple_time = "#{date} #{time}"
+
+    content_tag(:time, simple_time, options.merge(datetime: time.to_i, title: simple_time))
+  end
 end
